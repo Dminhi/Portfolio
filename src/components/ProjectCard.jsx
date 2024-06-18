383a49f9ffd2ec43aa7e68caf3d711d2b51f1d77
duyminh <hse.duyminh@gmail.com>
2024-06-18 16:47:24 +0700

    update

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = ({ project }) => {
       <img
         src={project.image}
         alt={project.title}
-        className="h-full sm:h-48 md:h-54 lg:h-60 xl:h-66 object-content"
+        className="w-full sm:h-48 md:h-54 lg:h-60 xl:h-66 object-content"
       />
       <div className="p-4 sm:p-6 md:p-8">
         <h3 className="text-lg sm:text-xl md:text-2xl font-semibold">
